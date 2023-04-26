7dcfb27925d867aa250a1d200ba2be369768754a
Omiya <90664344+OmiduAnjane@users.noreply.github.com>
2023-04-26 22:13:47 +0530

    👍

---
+++ test.js
@@ -0,0 +1,35 @@
+const getRecentMatches = require('./bin/recent_match');
+
+const getLiveScore = require('./bin/live_score');
+
+module.exports = {
+
+  getRecentMatches,
+
+  getLiveScore,
+
+}
+
+// getRecentMatches()
+
+// .then(data => {
+
+//   console.log(JSON.stringify({getRecentMatches:data}, null, 2));
+
+// })
+
+getLiveScore(21538)
+
+.then(data => {
+
+  console.log('\n\n\n\n\n\n');
+
+  console.log(JSON.stringify({getLiveScore:data}, null, 2));
+
+})
+
+setTimeout(() => {
+
+    
+
+}, 20000);
